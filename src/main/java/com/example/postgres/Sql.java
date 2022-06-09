76470fd9bf5c25f073798f78745966ff34cb30cc
hailaliyajacob <hailaliyajacob@gmail.com>
2022-06-09 14:08:40 +0530

    changing sql

--- src/main/java/com/example/postgres/Sql.java
+++ src/main/java/com/example/postgres/Sql.java
@@ -3,7 +3,7 @@ package com.example.postgres;
 public  class Sql {
 
     public static final String GET_SUMMARY =  "SELECT "+
-            "ID,FIRSTNAME, FROM MRISK ";
+            "ID,FIRSTNAME FROM MRISK ";
 
 
 
